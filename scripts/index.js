e65605089ff65503a340c5a1d007b0d0d3836896
Anna <rihisime@gmail.com>
2025-01-31 00:18:10 +0000

    :recycle: Обернула вывод карточек в функцию, исправила поиск элементов в DOM

--- scripts/index.js
+++ scripts/index.js
@@ -9,13 +9,17 @@ const getCardTemplate = (cardData, removeCard) => {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImage = cardElement.querySelector('.card__image');
     deleteButton.addEventListener('click', removeCard);
-    cardElement.querySelector('.card__image').src = cardData.link;
-    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
     return cardElement;
 }
 
+const renderCards = () => {
+    initialCards.forEach((cardData) => {
+        cardContainer.append(getCardTemplate(cardData, removeCard));
+    });
+}
 
-initialCards.forEach((cardData) => {
-    cardContainer.append(getCardTemplate(cardData, removeCard));
-});
+renderCards();
